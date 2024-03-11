d1965311e5f88aa721e665ccbcc3f02e8ae485ec
Erica Sadun <erica.sadun@temporal.io>
2024-03-11 14:47:33 -0600

    Cloud coin flipper with client app

--- CoinFlipperWorker.java
+++ CoinFlipperWorker.java
@@ -3,8 +3,8 @@ import io.temporal.worker.*;
 import io.temporal.client.WorkflowClient;
 import io.temporal.serviceclient.WorkflowServiceStubs;
 
-public class TutorialWorker {
-    public static void runTutorialWorker(String[] args) {
+public class CoinFlipperWorker {
+    public static void runCoinFlipperWorker(String[] args) {
         // Create a service stub
         WorkflowServiceStubs service = WorkflowServiceStubs.newLocalServiceStubs();
         
@@ -15,15 +15,17 @@ public class TutorialWorker {
         WorkerFactory factory = WorkerFactory.newInstance(client);
         
         // A Worker listens to one task queue, processing workflows and activities.
-        Worker worker = factory.newWorker("TutorialWorkflow-queue");
+        Worker worker = factory.newWorker("CoinFlipperWorkflow-queue");
 
         // Register a Workflow implementation with this worker.
         // The implementation must be known at runtime to dispatch workflow tasks.
-        worker.registerWorkflowImplementationTypes(TutorialWorkflow.TutorialWorkflowImpl.class);
+        worker.registerWorkflowImplementationTypes(
+                CoinFlipperWorkflow.CoinFlipperWorkflowImpl.class);
         
         // Register Activity Types from the Activity vendor with the worker.
         // Each activity is stateless and thread-safe, so use a single shared instance.
-        worker.registerActivitiesImplementations(new TutorialActivities.TutorialActivitiesImpl());
+        worker.registerActivitiesImplementations(
+                new CoinFlipperActivities.CoinFlipperActivitiesImpl());
         
         // Start all registered workers. The workers will start polling.
         factory.start();
